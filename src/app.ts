16ef73d4a5a840ace4cb80f6748031feec494e0e
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-01-27 23:33:33 +0600

    better auth init

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application } from 'express'
-
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.get('/', (req, res) => {
     res.send("Hello, world!")
