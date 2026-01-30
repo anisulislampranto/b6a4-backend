4a6494e53774a7e5d46ad6f416c58707b36a206d
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-01-30 21:55:55 +0600

    user update, get all user restricted to admin only

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -4,8 +4,8 @@ import auth, { UserRole } from "../../middleware/auth";
 
 const router = Router();
 
-router.get("/", UserController.getAllUsers)
-router.patch("/:id", UserController.updateUser)
+router.get("/", auth(UserRole.ADMIN), UserController.getAllUsers)
+router.patch("/:id", auth(UserRole.ADMIN), UserController.updateUser)
 router.get("/me", auth(UserRole.ADMIN, UserRole.CUSTOMER, UserRole.SELLER), UserController.getMe)
 
 export const userRouter: Router = router;
