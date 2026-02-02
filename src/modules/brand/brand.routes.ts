2c498ef9e766192c76db459f634a4d094d7d7135
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-02-02 11:28:18 -0500

    seller && admin can create brand

--- src/modules/brand/brand.routes.ts
+++ src/modules/brand/brand.routes.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 import { BrandController } from "./brand.controller";
+import auth, { UserRole } from "../../middleware/auth";
 
 const router = express.Router();
 
-router.post("/", BrandController.createBrand);
+router.post("/", auth(UserRole.ADMIN, UserRole.SELLER), BrandController.createBrand);
 router.get("/", BrandController.getBrands);
 
 export const brandRouter: Router = router;
